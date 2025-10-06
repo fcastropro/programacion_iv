cbcbde1269714295f3c6379b00fba5eb34ab97ef
Fernando Castro <fcastropro@gmail.com>
2025-10-06 17:54:45 -0500

    primer comitt motos

--- modulo_1/kotlin/app/src/main/java/com/example/basics/03_operadores_aritmeticos_mp.kt
+++ modulo_1/kotlin/app/src/main/java/com/example/basics/03_operadores_aritmeticos_mp.kt
@@ -1,14 +1,27 @@
 package com.example.basics
 
-fun main(){
-    println("!Operadores Aritmeticos!!!")
-    val value1: Double = 25.8
-    val value2: Double = 3.0
 
-    println("Suma: ${value1 + value2}")
-    println("Resta: ${value1 - value2}")
-    println("División: ${value1 / value2}")
-    println("Multiplicación: ${value1 * value2}")
+fun main() {
+    println("¡Bienvenido al sistema de Ventas de Motos!")
 
-    
+    val precioMoto: Double = 3200.0
+    val descuento: Double = 0.10   // 10% de descuento
+    val iva: Double = 0.15         // 15% de IVA
+
+    println("--- Calculos de Venta ---")
+    println("Precio base de la moto: $${precioMoto}")
+
+    val valorDescuento = precioMoto * descuento
+    println("Descuento aplicado (10%): $${valorDescuento}")
+
+    val precioConDescuento = precioMoto - valorDescuento
+    println("Precio con descuento: $${precioConDescuento}")
+
+    val valorIVA = precioConDescuento * iva
+    println("IVA (15%): $${valorIVA}")
+
+    val precioFinal = precioConDescuento + valorIVA
+    println("Precio final a pagar: $${precioFinal}")
+
+    println("¡Gracias por su compra en MotoShop!")
 }
